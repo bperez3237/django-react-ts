a45f28eebcb306870cf60300bc2f4f23bb0eb456
brian <b.perez3237@gmail.com>
2023-05-17 13:59:13 -0400

    Add server to python path in wsgi

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -8,6 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Get the root directory of your Django project
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add the "server" directory to the Python path
+APPS_DIR = os.path.join(ROOT_DIR, 'server')
+sys.path.append(APPS_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
